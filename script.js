cb6007cbda858bcf4f675ce9c8f0db10c4db2baf
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-05 01:10:22 +0430

    find difference between two string

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-document.getElementById("Result").innerHTML = t; 
+
+
+
+
+function getDifference(s, t)
+    {
+        var i = 0;
+        var j = 0;
+        var result = "";
+
+        while (j < t.length)
+        {
+         if (s[i] != t[j] || i == s.length)
+             result += t[j];
+         else
+             i++;
+         j++;
+        }
+        document.getElementById("Result").innerHTML = result;
+        return result;
+
+    }
+    console.log(getDifference("mary", "maryam"));
+
+
+     
 
 
  
