91772ce29f2dd87f1326659ec8c4584dd21f5cc5
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-05 11:31:51 +0430

    add length  check

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-
-
-
-
 function Difference(s, t)
     {
         var i = 0;
         var j = 0;
         var result = "";
 
-        while (j < t.length)
-        {
-         if (s[i] != t[j] || i == s.length)
-             result += t[j];
-         else
-             i++;
-         j++;
+        if( ( s.length <= 1000 ) && (t.length == s.length+1) ) {
+        	while (j < t.length)
+	        {
+	         if (s[i] != t[j] || i == s.length)
+	             result += t[j];
+	         else
+	             i++;
+	         j++;
+	        }
+	        document.getElementById("Result").innerHTML = result;
+	        return result;
+        } else {
+        	document.getElementById("Result").innerHTML = "Invalid strings!";
         }
-        document.getElementById("Result").innerHTML = result;
-        return result;
-
     }
-    console.log(Difference("mary", "maryam"));
+    console.log(Difference("abcd", "abcde"));
 
 
      
